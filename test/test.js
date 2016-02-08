37091723e92ce7e819a8445ff512c3ee12019671
Waylon Flinn <waylonflinn@gmail.com>
2016-02-08 16:26:39 -0600

    basic test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,11 @@
-var test = require('tape');
+var tape = require('tape'),
+	loader = require('../lib/xhr-loader.js');
+
+tape("load", function(t){
+	t.plan(1);
+
+	loader.load('./test/data/a.buf', Float32Array, function(err, result){
+		t.assert("lengths equal", result.length === 4096);
+	});
+
+});
