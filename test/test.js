ba58fc2197699e7bab5b96c0d870375917e2fce7
Waylon Flinn <waylonflinn@gmail.com>
2017-09-08 19:10:37 -0500

    api tests for loadall

--- test/test.js
+++ test/test.js
@@ -572,7 +572,6 @@ tape("loadall: files", function(t){
 	var files = ['a.json', 'a.f32', 'c.u8'];
 	var prefix = './test/data/';
 	var urls = files.map(function(file){ return prefix + file});
-	var expected = ['json', 'float32', 'uint8']; //
 
 	loader.loadall(urls, function(err, results){
 		var result = results[0];
@@ -586,5 +585,32 @@ tape("loadall: files", function(t){
 		result = results[2];
 		t.equal(type(result), 'Uint8Array', 'type correct');
 		t.assert(close(result[3], 4), 'value correct');
+	});
+});
+
+// test type on keyed columns
+tape("loadall: api", function(t){
+	t.plan(8);
+
+	var files = ['float32', 'int8', 'json', 'uint16'];
+	var prefix = 'http://localhost:8080/data/';
+	var urls = files.map(function(file){ return prefix + file});
+
+	loader.loadall(urls, function(err, results){
+		var result = results[0];
+		t.equal(type(result), 'Float32Array', 'type correct');
+		t.assert(close(result[2], 3.141592), 'value correct');
+
+		result = results[1];
+		t.equal(type(result), 'Int8Array', 'type correct');
+		t.assert(close(result[4], 127), 'value correct');
+
+		result = results[2];
+		t.equal(type(result), 'Object', 'type correct');
+		t.equal(JSON.stringify(result), JSON.stringify({"hello":"world"}), 'value correct');
+
+		result = results[3];
+		t.equal(type(result), 'Uint16Array', 'type correct');
+		t.assert(close(result[4], 1024), 'value correct');
 	});
 });
